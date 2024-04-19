8cf411c61c3e982b9f496795f16978037bd47503
Kevin Gibson <ninjamaster0822@gmail.com>
2024-04-18 21:05:52 -0500

    done2

--- problem1.rb
+++ problem1.rb
@@ -1,10 +1,14 @@
 def extract_urls(text)
-    pattern = %r{https://\S+}
-    text.scan(pattern) do |url|
-      puts url
-    end
+  pattern = %r{https?://\S+}
+  text.scan(pattern) do |url|
+    puts url
   end
-  
-  sample_text = "Visit our site at http://www.example.org for more information. Check out our search page at https://example.com/search?q=ruby+regex. Don’t forget to ftp our resources at ftp://example.com/resources."
-  
-  extract_urls(sample_text)
+  pattern = %r{ftp?://\S+}
+  text.scan(pattern) do |url|
+      puts url
+  end
+end
+
+sample_text = "Visit our site at http://www.example.org for more information. Check out our search page at https://example.com/search?q=ruby+regex. Don’t forget to ftp our resources at ftp://example.com/resources."
+
+extract_urls(sample_text)
